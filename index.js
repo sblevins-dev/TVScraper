1f5680d2b722ace070b8845b7fa4d6e8123d44df
Stephen Blevins <scblevins27@gmail.com>
2022-06-27 16:47:19 -0400

    scrapes first page and its details page

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
-axios("https://www.bestbuy.com/site/promo/tvs-for-gaming")
+const URL = "https://www.bestbuy.com/site/promo/tvs-for-gaming";
+
+const getDescription = (url) =>
+  axios(url)
+    .then((response) => {
+      let tempList = [];
+
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const descContainer = $(".shop-product-description");
+
+      desc = descContainer.find(".product-description").text();
+
+      tempList.push(desc);
+
+      return tempList;
+    })
+    .catch((err) => console.log(err));
+
+let data = [];
+
+axios(URL)
   .then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
@@ -18,19 +40,46 @@ axios("https://www.bestbuy.com/site/promo/tvs-for-gaming")
     // Make changes here
     const skuList = $(".component-sku-list").children();
 
-    skuList
-      .children("div")
-      .children("ol")
-      .children("li")
-      .next()
-      .find(".shop-sku-list-item")
-      .each((i, el) => {
-        const sku = $(el).children('div').attr('data-sku-id')
-        const prodName = $(el).find('.information').find(".sku-title a").text();
-        const prodPrice = $(el).find('.price-block').find('.priceView-customer-price').children('span').text()
-        const splitPrice = prodPrice.split('Y')[0]
+    let objCall = async () =>
+      skuList
+        .children("div")
+        .children("ol")
+        .children("li")
+        .next()
+        .find(".shop-sku-list-item")
+        .each(async (i, el) => {
+          const sku = $(el).children("div").attr("data-sku-id");
+          const prodImg = URL + $(el).find(".image-link").attr("href");
+          const prodHref =
+            URL + $(el).find(".information").find(".sku-title a").attr("href");
+          const prodName = $(el)
+            .find(".information")
+            .find(".sku-title a")
+            .text();
+          const prodDesc = await getDescription(prodHref);
+          const prodPrice = $(el)
+            .find(".price-block")
+            .find(".priceView-customer-price")
+            .children("span")
+            .text();
+          const splitPrice = prodPrice.split("Y")[0];
 
-        list.push({ sku, prodName, splitPrice });
-      });
+          let details = {
+            sku,
+            prodImg,
+            prodHref,
+            prodName,
+            prodDesc,
+            splitPrice,
+          };
+
+          console.log(details)
+          list.push(details);
+        });
+
+    objCall();
+    data = list;
   })
   .catch((err) => console.log(err));
+
+  console.log(data)
